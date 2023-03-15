4fc2a6283642586ad616590ddbd248d1b3244c80
7db9a <>
2023-03-15 13:23:22 -0400

    chore: generate config.

---
+++ generate-config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+// Load the desired config file based on the environment
+const env = process.env.NODE_ENV || 'development';
+const configFilePath = path.join(__dirname, `config.${env}.json`);
+const config = require(configFilePath);
+
+// Generate a JavaScript file with the required variables
+const configJs = `const CONFIG = ${JSON.stringify(config)};`;
+fs.writeFileSync(path.join(__dirname, 'config.js'), configJs);
