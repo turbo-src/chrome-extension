0340fd1950207125a06d318ac6a2a260e614696f
7db9a <>
2023-07-26 16:55:54 -0400

    Socket url determined by mode.

--- src/socketConfig.js
+++ src/socketConfig.js
@@ -1,11 +1,30 @@
 const { io } = require("socket.io-client");
+const CONFIG = require('./config.js');
 
-const socket = io("http://localhost:4007/", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd"
-  }
+const url = CONFIG.url;
+
+// get url
+console.log('socket mode url:', url)
+if (url === "http://localhost:4000/graphql" || url === "http://localhost:4006/graphql") {
+  console.log('initialize socket')
+  socket = io("http://localhost:4007/", {
+    withCredentials: true,
+    extraHeaders: {
+      "my-custom-header": "abcd"
+    }
+  });
+  console.log('socket created')
+} else {
+  console.log('initialize socket - online')
+
+  console.log('socket url:', url + ":4007")
+  socket = io(url + ":4007", {
+    withCredentials: true,
+    extraHeaders: {
+      "my-custom-header": "abcd"
+    }
+  });
+  console.log('socket created - online')
 }
-  );
 
-  module.exports = { socket }
+module.exports = { socket }
