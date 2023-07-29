cc8e9aa3d1524e32926c7fd6ade296429e54b381
7db9a <>
2023-07-29 16:29:50 -0400

    Make sure it doesn't reject because of missing intermediate ca on official endpoint.

--- src/socketConfig.js
+++ src/socketConfig.js
@@ -4,6 +4,7 @@ const CONFIG = require('./config.js');
 const url = CONFIG.url;
 
 console.log('socket mode url:', url)
+let socket;
 if (url.includes("localhost")) {
   console.log('initialize socket')
   socket = io("http://localhost:4007/", {
@@ -16,13 +17,44 @@ if (url.includes("localhost")) {
 } else {
   console.log('initialize socket - online')
   console.log('socket url:', url + "/socket.io")
-  socket = io(url + "/socket.io", {
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }
+  socket = io(url, {
+    path: '/socket.io',
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          'my-custom-header': 'abcd'
+        }
+      }
+    },
+    transports: ['websocket'], // <---- Add this line
+    secure: true,  // ensure secure connection
+    rejectUnauthorized: false  // ignore self-signed certificates
   });
-  console.log('socket created - online')
+
+  //socket = io(url, {
+  //  path: '/socket.io',
+  //  transportOptions: {
+  //    polling: {
+  //      extraHeaders: {
+  //        'my-custom-header': 'abcd'
+  //      }
+  //    }
+  //  },
+  //  secure: true,  // ensure secure connection
+  //  rejectUnauthorized: false  // ignore self-signed certificates
+  //});
+
+  socket.on('connect', () => {
+    console.log('Socket connection established');
+  });
+
+  socket.on('connect_error', (error) => {
+    console.error('Socket connection error:', error);
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log('Socket disconnected:', reason);
+  });
 }
 
 module.exports = { socket }
