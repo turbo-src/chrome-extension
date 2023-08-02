8ac3e3920570e395723bdc65a211b70c8b9dd707
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2023-08-02 17:16:10 -0400

    createRepo 201

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -73,8 +73,99 @@ describe('Auth', () => {
           }
           `
       }
+    });
+
+    cy.get('#turbo-src-btn-issue_6').should('be.visible');
+    cy.get('#turbo-src-btn-issue_5').should('be.visible');
+    cy.get('#turbo-src-btn-issue_4').should('be.visible');
+    cy.get('#turbo-src-btn-issue_3').should('be.visible');
+    cy.get('#turbo-src-btn-issue_2').should('be.visible');
+    cy.get('#turbo-src-btn-issue_1').should('be.visible');
+
+    cy.get('#turbo-src-btn-issue_1')
+      .contains('vote')
+      .click();
+  });
+});
+describe('Auth', () => {
+  it('successfully authenticates user', () => {
+    cy.visit(`https://github.com/login/oauth/authorize?scope=user:email&client_id=b0d124a1789132a87678`);
+
+    cy.get('input#login_field').type(Cypress.env('gitHubUsername'));
+    cy.get('input#password').type(Cypress.env('gitHubPassword'), {
+      log: false
+    });
+    cy.get('[type="submit"]').click();
+
+    // If GitHub prompts to accept terms:
+    // cy.get('[type="submit"]')
+    //   .contains('Authorize reibase')
+    //   .click();
+
+    cy.visit(`https://github.com/${Cypress.env('gitHubUsername')}/${Cypress.env('gitHubRepo')}/pulls`);
+
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:4000/graphql',
+      body: {
+        operationName: 'findOrCreateUser',
+        query: `
+        query findOrCreateUser {
+          findOrCreateUser(
+            turboSrcID: "${Cypress.env('turboSrcID')}",
+            owner: "",
+            repo: "",
+            contributor_id: "none",
+            contributor_name: "${Cypress.env('gitHubUsername')}",
+            contributor_signature: "none",
+            token: "${Cypress.env('gitHubToken')}"
+          ) 
+          { contributor_name, contributor_id }
+        }
+        `
+      }
     }).then(response => {
+      cy.log('response', response.body.data.findOrCreateUser.contributor_id);
+      expect(response.body.data.findOrCreateUser).to.have.property('contributor_name', Cypress.env('gitHubUsername'));
+      // Set contributor id and name for inject.js to refer to
+      window.localStorage.setItem('contributor_name', response.body.data.findOrCreateUser.contributor_name);
+      window.localStorage.setItem('contributor_id', response.body.data.findOrCreateUser.contributor_id);
+      // Set contributor id for the next tests
+      Cypress.env('contributorID', response.body.data.findOrCreateUser.contributor_id);
+      // Set owner and repo for future use and for inject.js to refer to
+      cy.get(':nth-child(1) > .AppHeader-context-item > .AppHeader-context-item-label').then(owner =>
+        window.localStorage.setItem('owner', owner[0].innerText)
+      );
+      cy.get(':nth-child(2) > .AppHeader-context-item > .AppHeader-context-item-label').then(repo =>
+        window.localStorage.setItem('repo', repo[0].innerText)
+      );
+    });
+
+    cy.visit(`https://github.com/${Cypress.env('gitHubUsername')}/${Cypress.env('gitHubRepo')}/pulls`);
+    cy.log('cont id', Cypress.env('contributorID'));
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:4000/graphql',
+      body: {
+        operationName: 'createRepo',
+        query: `
+          query createRepo {
+            createRepo(
+              turboSrcID: "${Cypress.env('turboSrcID')}",
+              owner: "${Cypress.env('gitHubUsername')}",
+              repo: "${Cypress.env('gitHubRepo')}",
+              defaultHash: "",
+              contributor_id: "${Cypress.env('contributorID')}",
+              side: "",
+              token: "${Cypress.env('gitHubToken')}"
+            ) 
+          }
+          `
+      }
+    }).then(response => {
+      //If response is null it is often because there is an issue with the contributor id
       console.log(response);
+      expect(response.body.data.createRepo, '201')
     });
 
     cy.request({
@@ -98,12 +189,7 @@ describe('Auth', () => {
           `
       }
     }).then(response => {
-      console.log(Cypress.env('turboSrcID'));
-      console.log(Cypress.env('contributorID'), 'contributorID');
-      console.log(Cypress.env('gitHubRepo'), 'gitHubRepo');
-
-      console.log(response.body, 'response.body.data.getRepoData');
-      expect(response.body.data.getRepoData).to.have.property('owner');
+      expect(response.body.data.getRepoData).to.have.property('contributor_id', Cypress.env('contributorID'));
     });
 
     cy.get('#turbo-src-btn-issue_6').should('be.visible');
@@ -112,9 +198,6 @@ describe('Auth', () => {
     cy.get('#turbo-src-btn-issue_3').should('be.visible');
     cy.get('#turbo-src-btn-issue_2').should('be.visible');
     cy.get('#turbo-src-btn-issue_1').should('be.visible');
-
-    cy.get('#turbo-src-btn-issue_1')
-      .contains('vote')
-      .click();
+    cy.get('#turbo-src-btn-issue_1').should('be.visible');
   });
 });
