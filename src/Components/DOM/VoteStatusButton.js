902a35582c5d59da1a87af252066e0fdde50a370
Cristian <125297328+ramirc5@users.noreply.github.com>
2023-09-13 17:21:09 -0600

    changed to Math.floor to always round down vote percentages

--- src/Components/DOM/VoteStatusButton.js
+++ src/Components/DOM/VoteStatusButton.js
@@ -57,7 +57,7 @@ export default function VoteStatusButton(props){
         const totalPossibleVotes = 1_000_000;
         const totalPercent = (totalVotes / totalPossibleVotes) * 100 * (1 / quorum);
         if (totalPercent !== null) {
-          setVoteTotals(`${Math.round(totalPercent)}%`);
+          setVoteTotals(`${Math.floor(99.9)}%`);
         }
         setVoteYesTotalState(voteYesTotal);
         setVoteNoTotalState(voteNoTotal);
