1772179b4cba28155f7b6ac9d84e94492b864593
Jeffrey Wood <jeffreywood@Jeffreys-MacBook-Air.local>
2023-10-31 15:20:18 -0400

    new ui with progress bar

--- src/Components/DOM/VoteStatusButton.js
+++ src/Components/DOM/VoteStatusButton.js
@@ -1,7 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
 import useGetVotes from '../../hooks/useGetVotes.js';
 import Skeleton from '@mui/material/Skeleton';
+import styled from 'styled-components';
+import ButtonProgress from './ButtonProgress.js';
+const LockIcon = 'https://www.reibase.rs/lock.png';
+
+const ButtonVote = styled.button`
+  color: white;
+  width: 80px;
+  height: 30px;
+  border: 0px;
+  font-weight: 500;
+  border-radius: 5px;
+  filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.35));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--button-color);
+  transition: background-color 0.3s ease;
+  margin-bottom: ${props => (props.zeroVotes ? '14px' : '0')};
+
+  &:hover {
+    background-color: var(--button-dark-color);
+  }
+`;
+
+const VoteButtonText = styled.span`
+  position: relative;
+  right: 7px;
+`;
+
+const IconImg = styled.img`
+  width: 16px;
+  height: 11px;
+  position: relative;
+  right: 15px;
+`;
+
+const Placeholder = styled.div`
+  width: 16px;
+  height: 11px;
+`;
 
 export default function VoteStatusButton({
   user,
@@ -9,28 +48,33 @@ export default function VoteStatusButton({
   issueID,
   contributorID,
   side,
-  modalOpen,
+  clicked,
   toggleModal,
   socketEvents
 }) {
   const [voteStatusButton, setVoteStatusButton] = useState({
-    color: 'lightgreen',
-    text: 'vote'
+    color: '#4AA0D5',
+    text: 'Vote'
   });
 
-  const { prData, loading } = useGetVotes(user, repoID, issueID, contributorID, side, socketEvents, modalOpen);
-  
+  const { prData, loading } = useGetVotes(user, repoID, issueID, contributorID, side, socketEvents, clicked);
+
   const buttonStyle = {
-    vote: ['lightgreen', 'vote'],
-    'pre-open': ['green', prData?.voteData?.voteTotals?.totalVotePercent + '%'],
-    open: ['orchid', prData?.voteData?.voteTotals?.totalVotePercent + '%'],
-    frozen: [
-      '#BFD4F2',
-      prData?.voteData?.voteTotals?.totalVotePercent > 0 ? prData?.voteData?.voteTotals?.totalVotePercent + '%' : 'vote'
-    ],
-    conflict: ['orange', 'conflict'],
-    merge: ['darkorchid', 'merged'],
-    close: ['red', 'closed']
+    vote: ['#4AA0D5', 'Vote'],
+    'pre-open': ['#4AA0D5', 'Vote'],
+    open: ['#4AA0D5', 'Vote'],
+    frozen: ['#919190', 'Vote'],
+    conflict: ['#FC9A28', 'Conflict'],
+    merge: ['#794D9A', 'Merged'],
+    close: ['#DD4747', 'Closed']
+  };
+
+  const transitionColors = {
+    '#4AA0D5': '#2391D6',
+    '#919190': '#82827E',
+    '#794D9A': '#66358B',
+    '#DD4747': '#C32424',
+    '#FC9A28': '#F1880C'
   };
 
   useEffect(() => {
@@ -51,8 +95,28 @@ export default function VoteStatusButton({
   }
 
   return (
-    <Button style={{ color: 'white', background: voteStatusButton.color, width: '80px' }} onClick={e => handleClick(e)}>
-      {voteStatusButton.text}
-    </Button>
+    <>
+      <ButtonVote
+        zeroVotes={prData.voteData.voteTotals.totalVotes === 0}
+        style={{
+          '--button-color': voteStatusButton.color,
+          '--button-dark-color': transitionColors[voteStatusButton.color] || voteStatusButton.color
+        }}
+        onClick={e => handleClick(e)}
+      >
+        {prData.state === 'frozen' || prData.state === 'conflict' ? (
+          <IconImg src={LockIcon} alt={prData.state} />
+        ) : (
+          <Placeholder />
+        )}
+        <VoteButtonText>{voteStatusButton.text}</VoteButtonText>
+      </ButtonVote>
+      <ButtonProgress
+        yesPercent={prData.voteData.voteTotals.yesPercent}
+        noPercent={prData.voteData.voteTotals.noPercent}
+        quorum={prData.voteData.voteTotals.quorum}
+        totalVotes={prData.voteData.voteTotals.totalVotes}
+      />
+    </>
   );
 }
