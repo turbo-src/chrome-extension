273d7bf3f0910c9947f6bc230c28d63399a7db8a
7db9a <>
2023-11-07 15:21:23 -0500

    Prepend nonce to encrypted payload.

--- sharedSecret.js
+++ sharedSecret.js
@@ -1,21 +1,18 @@
 const crypto = require('crypto');
 const secp256k1 = require('secp256k1');
-const ethUtil = require('ethereumjs-util');
 
 // Your private key (must be 32 bytes hex string without 0x prefix)
-const clientEthereumPrivateKeyHex = '..';
+const clientEthereumPrivateKeyHex = '...';
 
 // Ensure the private key is a Buffer
 const clientEthereumPrivateKeyBuffer = Buffer.from(clientEthereumPrivateKeyHex, 'hex');
 
 // Assume you've received the other party's public key as a hex string
-const serverEthereumPublicKeyHex = '..'
+const serverEthereumPublicKeyHex = "..."
 const serverEthereumPublicKeyBuffer = Buffer.from(serverEthereumPublicKeyHex, 'hex');
 
 // Generate the shared secret
 const sharedSecret = secp256k1.ecdh(serverEthereumPublicKeyBuffer, clientEthereumPrivateKeyBuffer);
-
-console.log('Shared secret:', sharedSecret.toString('hex'));
 
 // Create a SHA-256 hash of the shared secret to use as the key for AES-256
 const key = crypto.createHash('sha256').update(sharedSecret).digest();
@@ -29,11 +26,7 @@ const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 let encrypted = cipher.update(message, 'utf8', 'hex');
 encrypted += cipher.final('hex');
 
-console.log('Encrypted message:', encrypted);
+// Prepend the IV to the encrypted message as a hex string
+const payload = iv.toString('hex') + encrypted;
 
-// To decrypt the message later on:
-const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-
-console.log('Decrypted message:', decrypted);
+console.log('Payload with IV:', payload);
